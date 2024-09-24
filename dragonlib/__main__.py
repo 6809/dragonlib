ddf9b16a29ee61e8f0f1d4430ec96c7a6acb15c0
JensDiemer <git@jensdiemer.de>
2024-09-24 21:25:05 +0200

    Modernize Project Setup

---
+++ dragonlib/__main__.py
@@ -0,0 +1,11 @@
+"""
+    Allow dragonlib to be executable
+    through `python -m dragonlib`.
+"""
+
+
+from dragonlib.cli_app import main
+
+
+if __name__ == '__main__':
+    main()
---
+++ dragonlib/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+import dragonlib
+
+
+CLI_EPILOG = 'Project Homepage: https://github.com/6809/dragonlib'
+
+BASE_PATH = Path(dragonlib.__file__).parent
